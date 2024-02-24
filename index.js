68dafea2a5a4c775cd2f4735dabde92e090126b5
Rahul Sharma <rahul658541@gmail.com>
2024-02-25 00:45:48 +0530

    Change

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
-const express=require('express');
-const app=express();
-const bodyparser=require('body-parser');
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:true}));
-app.set('view engine','ejs');
-app.set('views','./Views')
-const https=require('https')
-let axios=require('axios')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const path = require('path');
+// Example static files configuration in Express.js
+app.use(express.static('public'));
 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'Views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/weather', async (req, res) => {
+  res.render('weather');
+});
 
-app.get('/weather', async(req,res)=>{
-
-res.render('weather')
-   
-})  
 app.post('/weather', async (req, res) => {
   try {
-      const weather = req.body.weather;
-      const apiKey = 'bf5dfdfae8848c9763983934045d2bb3'; // Replace with your OpenWeatherMap API key
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weather}&appid=${apiKey}`;
+    const weather = req.body.weather;
+    const apiKey = 'bf5dfdfae8848c9763983934045d2bb3'; // Replace with your OpenWeatherMap API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weather}&appid=${apiKey}`;
 
-     const response=await axios.get(apiUrl);
-     const weatherData=response.data
-    console.log(weatherData)
-      const tempdata = weatherData.main.temp;
-      let newtemp= (tempdata -273)
-      const name = weatherData.name;
-      const speed = weatherData.wind.speed;
+    const response = await axios.get(apiUrl);
 
-
-      res.render('weatherData', { temp: Math.floor(newtemp), name: name, speed: speed });
-  } catch (error) {
-      console.error(error);
+    if (!response || response.status !== 200) {
+      console.error('Error in API request:', response && response.statusText);
       res.status(500).send('Error fetching weather data');
+      return;
+    }
+
+    const weatherData = response.data;
+    const tempdata = weatherData.main.temp;
+    let newtemp = tempdata - 273;
+    const name = weatherData.name;
+    const speed = weatherData.wind.speed;
+
+    res.render('weatherData', { temp: Math.floor(newtemp), name: name, speed: speed });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching weather data');
   }
 });
-app.listen(5000,()=>{
-    console.log("server start on port 5000")
-})
 
-
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
